23e83fa421d0d67cc2b43c1863859dd174d75abb
Blondy <blondymartinezm@gmail.com>
2024-03-05 17:11:41 +0100

    added tests for board and player  initialization

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,17 +6,22 @@ RSpec.describe Game do
             game = Game.new
             expect(game).to be_an_instance_of(Game)
         end
-    end
 
-    describe "#initialize" do
         it "starts the game with an empty board" do
             game = Game.new
-            expect(game.board).to eq([[" ", " ", " ", " ", " ", " ", " "],
-                                    [" ", " ", " ", " ", " ", " ", " "],
-                                    [" ", " ", " ", " ", " ", " ", " "],
-                                    [" ", " ", " ", " ", " ", " ", " "],
-                                    [" ", " ", " ", " ", " ", " ", " "],
-                                    [" ", " ", " ", " ", " ", " ", " "]])
+            expect(game.board.board_state).to eq([[" ", " ", " ", " ", " ", " ", " "],
+                                                [" ", " ", " ", " ", " ", " ", " "],
+                                                [" ", " ", " ", " ", " ", " ", " "],
+                                                [" ", " ", " ", " ", " ", " ", " "],
+                                                [" ", " ", " ", " ", " ", " ", " "],
+                                                [" ", " ", " ", " ", " ", " ", " "]])
+        end
+
+        it "initializes player1" do
+            game = Game.new
+            player1 = game.player1
+            expect(player1.name).to eq('player1')
+            expect(player1.symbol).to eq('●'.green)
         end
     end
 end
